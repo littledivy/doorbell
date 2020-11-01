28221630c0f63757d77f73369d1563ff17b35ee6
divy-work <dj.srivastava23@gmail.com>
2020-11-01 17:55:14 +0530

    chores(dev): add serial monitor

--- doorbell.ino
+++ doorbell.ino
@@ -1,3 +1,8 @@
+// ---------------------------------------------------------------- //
+// Arduino Ultrasoninc Sensor HC-SR04
+// Using HC-SR04 Module
+// Nov 1 2020
+// ---------------------------------------------------------------- //
 
 #define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
@@ -10,8 +15,6 @@ void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
   Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
-  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
-  Serial.println("with Arduino UNO R3");
 }
 void loop() {
   // Clears the trigPin condition
@@ -24,9 +27,14 @@ void loop() {
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+  distance = duration * 0.034 / 2; 
+  if(distance < 10) {
+      Serial.print("Doorbell triggered at ");
+      Serial.print(distance);
+      Serial.println(" cm");
+  } else {
+        Serial.print("Distance: ");
+        Serial.print(distance);
+        Serial.println(" cm");
+  }
 }
